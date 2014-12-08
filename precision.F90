710729c3efc01ca1618a87bf7c998dd77141df3d
Pedro Brandimarte <brandimarte@gmail.com>
2014-12-08 11:59:35 -0200

    Initial commit with precision module

---
+++ precision.F90
@@ -0,0 +1,48 @@
+!  *******************************************************************  !
+!  KPM Fortran Code 2014                                                !
+!                                                                       !
+!  Written by Eric de Castro e Andrade (eandrade@ift.unesp.br) and      !
+!             Pedro Brandimarte (brandimarte@gmail.com).                !
+!                                                                       !
+!  Copyright (c), All Rights Reserved                                   !
+!                                                                       !
+!  This program is free software. You can redistribute it and/or        !
+!  modify it under the terms of the GNU General Public License          !
+!  (version 3 or later) as published by the Free Software Foundation    !
+!  <http://fsf.org/>.                                                   !
+!                                                                       !
+!  This program is distributed in the hope that it will be useful, but  !
+!  WITHOUT ANY WARRANTY, without even the implied warranty of           !
+!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU     !
+!  General Public License for more details (file 'LICENSE_GPL'          !
+!  distributed along with this program or at                            !
+!  <http://www.gnu.org/licenses/gpl.html>).                             !
+!  *******************************************************************  !
+!                           MODULE precision                            !
+!  *******************************************************************  !
+!  Description: define the precision of floating point numbers.         !
+!                                                                       !
+!  Written by Pedro Brandimarte, Dec 2014.                              !
+!  Instituto de Fisica                                                  !
+!  Universidade de Sao Paulo                                            !
+!  e-mail: brandimarte@gmail.com                                        !
+!  ***************************** HISTORY *****************************  !
+!  Original version:    December 2014                                   !
+!  *******************************************************************  !
+
+MODULE precision
+
+  implicit none
+
+  PUBLIC ! default is public
+
+  integer, parameter :: sp = selected_real_kind(6,30) ! single
+
+  integer, parameter :: dp = selected_real_kind(14,100) ! double
+
+
+!  *******************************************************************  !
+
+
+END MODULE precision
+
